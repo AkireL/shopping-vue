17f18c008f54c57d8e7d845a62fb6ffbd7c5e802
Erika Leonor Basurto Munguia <e.basurto@cyberpuerta.mx>
2025-01-05 17:15:53 -0600

    remove menu

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,6 @@
 </script>
 
 <template>
-  <h1>Hello App!</h1>
-  <p>
-    <!-- use the router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
-      <router-link to="/main">Main</router-link>
-      <br>
-      <router-link to="/about">About</router-link>
-      <br>
-      <router-link to="/row/1">Row</router-link>
-  </p>
   <!-- <router-view></router-view> -->
   <router-view></router-view>
 </template>
